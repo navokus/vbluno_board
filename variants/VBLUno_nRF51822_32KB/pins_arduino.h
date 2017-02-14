2204a6bb45b28268c9925f7a134bfb3c617b0c7d
thunguyen <thunta3@vng.com.vn>
2017-02-14 12:13:39 +0700

    first commit

---
+++ variants/VBLUno_nRF51822_32KB/pins_arduino.h
@@ -0,0 +1,58 @@
+
+#ifndef PINS_ARDUINO_H_
+#define PINS_ARDUINO_H_
+
+#define VBLUNO_NRF51822
+
+//PIN Define
+#define D0 0
+#define D1 1
+#define D2 2
+#define D3 3
+#define D4 4
+#define D5 5
+#define D6 6
+#define D7 7
+#define D8 8
+#define D9 9
+#define D10 10
+#define D11 11
+#define D12 12
+#define D13 13
+#define D14 14
+#define D15 15
+#define D16 16
+#define D17 17
+#define D18 18
+#define D19 19
+#define D20 20
+#define D21 21
+
+#define SS   D10
+#define MOSI D11
+#define MISO D12
+#define SCK  D13
+
+#define A0 D14
+#define A1 D15
+#define A2 D16
+#define A3 D17
+#define A4 D18
+#define A5 D19
+
+#define SDA0  D20
+#define SCL0  D21
+
+#define SDA1  D18
+#define SCL1  D19
+
+#define DEFAULT_RX_PIN D0 
+#define DERAULT_TX_PIN D1
+
+#define TWI_SDA SDA0
+#define TWI_SCL SCL0
+
+#define LED 22
+
+#endif
+
